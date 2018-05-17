66e9f3489208b4b3a07060d95ab8fad524a827fb
Cryptophobia <aouzounov@gmail.com>
2018-05-17 16:52:26 -0400

    finishing seeding db and controllers and on to client

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,51 +1,15 @@
-class DrinksController < ApplicationController
+class DrinksController < ApiController
   before_action :set_drink, only: [:show, :update, :destroy]
 
   # GET /drinks
   def index
-    @drinks = Drink.all
-
-    render json: @drinks
+    @drinks = Drink.select("id, title").all
+    render json: @drinks.to_json
   end
 
-  # GET /drinks/1
+  # GET /drinks/:id
   def show
-    render json: @drink
+    @drinks = Drink.select(params[:id])
+    render json: @drink.to_json(:include => { :ingredients => { :only => [:id, :description] }})
   end
-
-  # POST /drinks
-  def create
-    @drink = Drink.new(drink_params)
-
-    if @drink.save
-      render json: @drink, status: :created, location: @drink
-    else
-      render json: @drink.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /drinks/1
-  def update
-    if @drink.update(drink_params)
-      render json: @drink
-    else
-      render json: @drink.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /drinks/1
-  def destroy
-    @drink.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_drink
-      @drink = Drink.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def drink_params
-      params.require(:drink).permit(:title, :description, :steps, :source)
-    end
 end
--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    window.fetch('/api/drinks')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error));
+  }
   render() {
     return (
       <div className="App">
